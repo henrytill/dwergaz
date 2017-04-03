321c3f4e47fa1fd4912cf920a803c49611d58313
Henry Till <henrytill@gmail.com>
2017-04-03 18:30:17 -0400

    add initial work

---
+++ src/Dwergaz.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE GADTs #-}
+
+module Dwergaz where
+
+data Test where
+  Predicate :: (Eq a, Show a) => String -> (a -> Bool) -> a -> Test
+  Expect    :: (Eq a, Show a) => String -> (a -> a -> Bool) -> a -> a -> Test
+
+data Result where
+  Passed :: String -> Result
+  Failed :: Show a => String -> a -> a -> Result
+
+instance Show Result where
+  show (Failed n e a) =
+    "FAILED:   "     ++ n      ++
+    "\nEXPECTED:   " ++ show e ++
+    "\nACTUAL:     " ++ show a
+  show (Passed n) =
+    "PASSED:   "     ++ n
+
+isPassed :: Result -> Bool
+isPassed (Passed _) = True
+isPassed _          = False
+
+runTest :: Test -> Result
+runTest (Predicate n p v)
+  | p v                   = Passed n
+  | otherwise             = Failed n True False
+runTest (Expect n f e a)
+  | f e a                 = Passed n
+  | otherwise             = Failed n e a
